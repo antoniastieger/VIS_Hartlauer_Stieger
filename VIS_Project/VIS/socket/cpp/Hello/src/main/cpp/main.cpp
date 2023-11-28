453ef6c5a7fd1103df4c4e5a0ba203950e7a3189
Antonia Stieger <s2210237014@fhooe.at>
2023-11-28 12:01:21 +0100

    preperations

--- VIS_Project/VIS/socket/cpp/Hello/src/main/cpp/main.cpp
+++ VIS_Project/VIS/socket/cpp/Hello/src/main/cpp/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream> // cout, cin
-#include <iostream> // cout, cin
 #include <sys/socket.h> // socket, bind, listen, accept
 #include <netinet/in.h> // IPPROTO_TCP, sockaddr_in,
                         // htons/ntohs, INADDR_ANY
@@ -7,15 +6,11 @@
 #include <arpa/inet.h> // inet_ntop/inet_atop
 #include <string.h> // strlen
 #include <semaphore.h> // sem_init
-#include <pthread.h>
-
-#include "Foo.h"
 
 #define BUFFER_SIZE 1024
 
 using namespace std;
 
 int main(int _argc, char** _argv) {
-	cout << "HelloWorld " << Foo::Bar() << endl;
-    return 0;
+	cout << "Hello World!" << endl;
 }
