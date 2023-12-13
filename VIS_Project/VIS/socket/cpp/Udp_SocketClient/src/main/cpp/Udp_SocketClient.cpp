25a367909a5cbf2e991330215af562cb8bea5853
Antonia Stieger <s2210237014@fhooe.at>
2023-12-13 11:35:19 +0100

    1.2b client

--- VIS_Project/VIS/socket/cpp/Udp_SocketClient/src/main/cpp/Udp_SocketClient.cpp
+++ VIS_Project/VIS/socket/cpp/Udp_SocketClient/src/main/cpp/Udp_SocketClient.cpp
@@ -1,3 +1,61 @@
 //
-// Created by sandr on 13.12.2023.
+// Created by Antonia Stieger on 13.12.2023.
 //
+
+#include <iostream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <cstring>
+
+#define BUFFER_SIZE 1024
+
+int main() {
+    // Create a socket
+    int udpClientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if (udpClientSocket == -1) {
+        std::cerr << "Error creating client socket" << std::endl;
+        return -1;
+    }
+
+    // Read a line from the command line
+    std::string input;
+    std::cout << "Enter a line: " << std::endl;
+    std::getline(std::cin, input);
+    input.append("\0");
+
+    // Send the line to the server
+    sockaddr_in toAddr;
+    toAddr.sin_family = AF_INET;
+    toAddr.sin_port = htons(4949);
+    toAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(&(toAddr.sin_zero),'\0',8);
+    int toSize = sizeof(sockaddr_in);
+
+    int sendRVal = sendto(udpClientSocket, input.c_str(), input.size(), 0, (sockaddr*)&toAddr, toSize);
+    if (sendRVal == -1) {
+        std::cerr << "Error sending data" << std::endl;
+    } else {
+        std::cout << "Sent " << sendRVal << " bytes of data" << std::endl;
+    }
+
+    // Receive acknowledgment from the server
+    char ackBuffer[BUFFER_SIZE];
+    sockaddr_in from;
+    int fromSize = sizeof(sockaddr_in);
+
+    int recvRVal = recvfrom(udpClientSocket, ackBuffer, BUFFER_SIZE, 0, (sockaddr *)&from, (socklen_t *)&fromSize);
+
+    if (recvRVal == -1) {
+        std::cerr << "Error receiving acknowledgment" << std::endl;
+    } else {
+        std::cout << "Received acknowledgment from server: " << ackBuffer << std::endl;
+    }
+
+    // Close the socket
+    close(udpClientSocket);
+
+    return 0;
+};
+
+
