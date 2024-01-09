f5417793b5c0a9d673b52f98349955d8c6b4b28d
Antonia Stieger <s2210237014@fhooe.at>
2024-01-09 11:51:25 +0100

    EX2 warm up: RmiServer

---
+++ VIS_Project/VIS/rmi/Hello_RmiServer.java
@@ -0,0 +1,27 @@
+//
+// Created by Antonia Stieger on 09.01.24.
+//
+
+import java.rmi.*;
+
+public class Hello_RmiServer
+    extends UnicastRemoteObject
+    implements Hello_RmiInterface {
+
+    public Hello_RmiServer() throws RemoteException { super(); }
+
+    public static void main(String[] _args) {
+        try {
+            Hello_RmiServer server = new Hello_RmiServer();
+            Registry reg = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+            reg.rebind("Hello", server);
+            System.out.println("Server is waiting for queries ...");
+        } catch (Exception _e) {
+            System.out.println("Server failed: " + _e);
+        }
+    }
+
+    public String saySomething() throws RemoteException {
+        return "cookies!";
+    }
+}
