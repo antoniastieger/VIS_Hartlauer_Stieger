64bb0a1ebe06f1da57cb2431b23fce4ebf2931a6
Sandra <s2210237030@fhooe.at>
2024-01-10 10:01:57 +0100

    EX02 WarmUp JavaDoc

--- VIS_Project/VIS/rmi/Hello_RmiInterface/src/main/java/at.fhooe.sail.vis/Hello_RmiInterface.java
+++ VIS_Project/VIS/rmi/Hello_RmiInterface/src/main/java/at.fhooe.sail.vis/Hello_RmiInterface.java
@@ -4,7 +4,18 @@
 package at.fhooe.sail.vis;
 
 import java.rmi.*;
+
+/**
+ * Remote interface for RMI communication.
+ */
 public interface Hello_RmiInterface extends Remote {
+
+    /**
+     * Returns a greeting or message from the remote object.
+     *
+     * @return a String representing the greeting or message.
+     * @throws RemoteException if there is a communication-related issue during the remote method invocation.
+     */
     public String saySomething() throws RemoteException;
 
 }
--- VIS_Project/VIS/rmi/Hello_RmiServer/src/main/java/at.fhooe.sail.vis/Hello_RmiServer.java
+++ VIS_Project/VIS/rmi/Hello_RmiServer/src/main/java/at.fhooe.sail.vis/Hello_RmiServer.java
@@ -9,13 +9,33 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+/**
+ * RMI server providing a simple greeting service.
+ */
 public class Hello_RmiServer
     extends UnicastRemoteObject
     implements Hello_RmiInterface {
 
+    /**
+     * Default constructor for creating the RMI server.
+     *
+     * @throws RemoteException if an RMI communication-related issue occurs.
+     */
     public Hello_RmiServer() throws RemoteException { super(); }
+
+    /**
+     * Provides a greeting or message.
+     *
+     * @return a String representing the greeting message.
+     * @throws RemoteException if an RMI communication-related issue occurs.
+     */
     public String saySomething() throws RemoteException { return "cookies!"; }
 
+    /**
+     * Starts the RMI server and binds it to the registry.
+     *
+     * @param _args command-line arguments (not used).
+     */
     public static void main(String[] _args) {
         try {
             Hello_RmiServer server = new Hello_RmiServer();
