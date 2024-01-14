b7d43b96cec63f461245277755f5ab4290f87fe3
Antonia Stieger <s2210237014@fhooe.at>
2024-01-14 10:48:13 +0100

    EX2 2.2a und b finished and tested

--- VIS_Project/VIS/servlet/Hello_Servlet/src/main/java/HelloWorldServlet.java
+++ VIS_Project/VIS/servlet/Hello_Servlet/src/main/java/HelloWorldServlet.java
@@ -2,7 +2,6 @@
 // Created by Antonia Stieger on 10.01.2024.
 //
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,16 +9,37 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * This servlet class, HelloWorldServlet, is a simple example demonstrating a hit counter.
+ * It extends HttpServlet to handle HTTP requests.
+ * It is annotated with @WebServlet to specify its name and URL patterns for mapping.
+ * The servlet maintains a hit count, which increments with each incoming request.
+ */
 @WebServlet(name = "No of hits", urlPatterns = {"/hits", "/hitCounter"})
 public class HelloWorldServlet extends HttpServlet {
+    /**
+     * The hitCount variable stores the total number of hits to this servlet.
+     */
     private int hitCount;
 
+    /**
+     * Initializes the servlet by setting the hitCount to 0.
+     */
     public void init() {
         hitCount = 0;
     }
 
-    @Override public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    /**
+     * Handles HTTP GET requests. Increments the hit count and sends a response
+     * displaying the total number of hits.
+     *
+     * @param request  the HttpServletRequest object containing the request parameters
+     * @param response the HttpServletResponse object for sending the responses
+     * @throws IOException      if an I/O error occurs during the processing of the request
+     */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
         hitCount++;
 
         response.setContentType("text/html");
