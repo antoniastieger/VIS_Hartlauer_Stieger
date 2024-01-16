9853b2693d7d391716e57a8ae71b6de5ece7f2af
Sandra <s2210237030@fhooe.at>
2024-01-16 12:49:28 +0100

    Ex3 3.1c finished with JavaDoc

--- VIS_Project/VIS/jaxb/parser/JSON/HighscoreParser/src/main/java/at.fhooe.sail.vis/HighscoreEntry.java
+++ VIS_Project/VIS/jaxb/parser/JSON/HighscoreParser/src/main/java/at.fhooe.sail.vis/HighscoreEntry.java
@@ -1,25 +1,63 @@
 package at.fhooe.sail.vis;
 
+/**
+ * The HighscoreEntry class represents an entry in the highscore list,
+ * containing information such as name, score, and time.
+ */
 public class HighscoreEntry {
-    private String name;
-    private int score;
-    private int time;
 
-    public HighscoreEntry(String name, int score, int time) {
-        this.name = name;
-        this.score = score;
-        this.time = time;
+    /**
+     * The name associated with the highscore entry.
+     */
+    private String mName;
+
+    /**
+     * The score associated with the highscore entry.
+     */
+    private int mScore;
+
+    /**
+     * The time associated with the highscore entry.
+     */
+    private int mTime;
+
+    /**
+     * Constructs a HighscoreEntry with the specified name, score, and time.
+     *
+     * @param _name  The name associated with the highscore entry.
+     * @param _score The score associated with the highscore entry.
+     * @param _time  The time associated with the highscore entry.
+     */
+    public HighscoreEntry(String _name, int _score, int _time) {
+        this.mName = _name;
+        this.mScore = _score;
+        this.mTime = _time;
     }
 
-    public String getName() {
-        return name;
+    /**
+     * Gets the name associated with the highscore entry.
+     *
+     * @return The name of the highscore entry.
+     */
+    public String getmName() {
+        return mName;
     }
 
-    public int getScore() {
-        return score;
+    /**
+     * Gets the score associated with the highscore entry.
+     *
+     * @return The score of the highscore entry.
+     */
+    public int getmScore() {
+        return mScore;
     }
 
-    public int getTime() {
-        return time;
+    /**
+     * Gets the time associated with the highscore entry.
+     *
+     * @return The time of the highscore entry.
+     */
+    public int getmTime() {
+        return mTime;
     }
 }
--- VIS_Project/VIS/jaxb/parser/JSON/HighscoreParser/src/main/java/at.fhooe.sail.vis/HighscoreParser.java
+++ VIS_Project/VIS/jaxb/parser/JSON/HighscoreParser/src/main/java/at.fhooe.sail.vis/HighscoreParser.java
@@ -7,12 +7,22 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The HighscoreParser class is responsible for parsing JSON strings containing highscore data
+ * and converting them into a list of HighscoreEntry objects.
+ */
 public class HighscoreParser {
 
-    public List<HighscoreEntry> parseHighscores(String jsonString) {
+    /**
+     * Parses the given JSON string to extract highscore entries.
+     *
+     * @param _jsonString The JSON string containing highscore data.
+     * @return A list of HighscoreEntry objects parsed from the JSON string.
+     */
+    public List<HighscoreEntry> parseHighscores(String _jsonString) {
         List<HighscoreEntry> highscores = new ArrayList<>();
 
-        JSONObject jsonObject = new JSONObject(jsonString);
+        JSONObject jsonObject = new JSONObject(_jsonString);
         if (jsonObject.has("Highscore")) {
             JSONArray highscoreArray = jsonObject.getJSONArray("Highscore");
 
@@ -30,7 +40,12 @@ public class HighscoreParser {
         return highscores;
     }
 
-    public static void main(String[] args) {
+    /**
+     * Example main method demonstrating the usage of the HighscoreParser class.
+     *
+     * @param _args Command-line arguments (not used in this example).
+     */
+    public static void main(String[] _args) {
         // Example usage
         String jsonString = "{" +
                 "\"Highscore\": [" +
@@ -45,7 +60,7 @@ public class HighscoreParser {
 
         // You can now work with the list of HighscoreEntry objects
         for (HighscoreEntry entry : highscores) {
-            System.out.println("Name: " + entry.getName() + ", Score: " + entry.getScore() + ", Time: " + entry.getTime());
+            System.out.println("Name: " + entry.getmName() + ", Score: " + entry.getmScore() + ", Time: " + entry.getmTime());
         }
     }
 }
