57b9d161419a8c3f329fe69f14e8c23545aac280
Sandra <s2210237030@fhooe.at>
2024-01-16 12:45:21 +0100

    Ex3 3.1c finished

--- VIS_Project/VIS/jaxb/parser/JSON/HighscoreParser/build.gradle.kts
+++ VIS_Project/VIS/jaxb/parser/JSON/HighscoreParser/build.gradle.kts
@@ -7,6 +7,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.json:json:20210307")
 }
 
 application {
--- VIS_Project/VIS/jaxb/parser/JSON/HighscoreParser/src/main/java/at.fhooe.sail.vis/HighscoreParser.java
+++ VIS_Project/VIS/jaxb/parser/JSON/HighscoreParser/src/main/java/at.fhooe.sail.vis/HighscoreParser.java
@@ -1,5 +1,6 @@
 package at.fhooe.sail.vis;
 
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
