e6a796f26616e86993456a31483cb930ab08c55e
Antonia Stieger <s2210237014@fhooe.at>
2024-01-16 11:11:57 +0100

    wupuuuwoouuu starting ex3

---
+++ VIS_Project/VIS/jaxb/BruteForce/src/main/java/WgsContainer.java
@@ -0,0 +1,26 @@
+//
+// Created by Antonia Stieger on 16.01.2024.
+//
+
+package BruteForce;
+
+public class Wgs84Container {
+    private String latitude;
+    private String longitude;
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
