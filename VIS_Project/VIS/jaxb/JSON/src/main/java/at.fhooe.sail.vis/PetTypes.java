3a911d938eca08dd6b6dd6957f19a507bdde1545
Antonia Stieger <s2210237014@fhooe.at>
2024-01-22 14:53:17 +0100

    tralalallalalalalla

---
+++ VIS_Project/VIS/jaxb/JSON/src/main/java/at.fhooe.sail.vis/PetTypes.java
@@ -0,0 +1,19 @@
+//
+// Created by Antonia Stieger on 16.01.2024.
+//
+
+package at.fhooe.sail.vis;
+
+/**
+ * Java class that holds enumeration representing different types of pets.
+ */
+public class PetTypes {
+
+    /**
+     * Enumeration representing different types of pets.
+     * Each constant corresponds to a specific type such as CAT, DOG, MOUSE, or BIRD.
+     */
+    public enum Type {
+        CAT, DOG, MOUSE, BIRD
+    }
+}
